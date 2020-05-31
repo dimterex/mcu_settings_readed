97d201bb487ba93d1f758b0f6c2a1021c66bd92c
dimterex <dimterex@mail.ru>
2020-05-31 17:45:18 +0700

    Fast app for android car system MCU

---
+++ app/src/main/java/com/dimterex/mcu_settings/MainActivity.java
@@ -0,0 +1,58 @@
+package com.dimterex.mcu_settings;
+
+import android.microntek.CarManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private CarManager mCarManager;
+    private EditText input_text;
+
+    private ListView output_list;
+    private ArrayList<String> listItems;
+    private ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listItems = new ArrayList<String>();
+        input_text = findViewById(R.id.input_text);
+        output_list = findViewById(R.id.output_list);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
+        output_list.setAdapter(adapter);
+
+        mCarManager = new CarManager();
+
+        Button read_button  = findViewById(R.id.read_button);
+        read_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (input_text == null)
+                    return;
+
+                if (output_list == null)
+                    return;
+
+                String input = input_text.getText().toString();
+
+                String parameter  = mCarManager.getParameters(input);
+                if (parameter == null)
+                    parameter = " failed to read";
+
+                listItems.add(input + parameter);
+                adapter.notifyDataSetChanged();
+                output_list.setSelection(listItems.size() - 1);
+            }
+        });
+    }
+}
